81c4d0344e4537059097f389d699f922fa4e50c8
Gaius G D'cruz <gaingaius123@gmail.com>
2021-03-13 00:40:15 +0530

    Completed

---
+++ Interview Preperation Kit/Warm-up Challenges/Counting-Valleys.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    count_d = 0
+    count_u = 0
+    for i in path:
+        if i == 'U':
+            count_u += 1
+            if count_u == 0:
+                count_d += 1
+        else:
+            count_u -= 1
+    return count_d
+      
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
